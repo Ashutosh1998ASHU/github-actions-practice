78848b5a72ca36b96d0e9aebdbb16e27eacba2ef
Ashutosh Chauhan <66657122+Ashutosh1998ASHU@users.noreply.github.com>
2026-06-20 11:33:37 +0530

    Linted Code , wrong syntax

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
-
-
+     return None
+    
+if nne:
+    go
+    
 @app.route('/health')
 def health():
     return 'Server is up and running'
